02ed2b8e0a21b90c261e7b6aabc64f04942e7eae
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-08 09:21:12 -0300

    Comentario e bibliotecas (C ANSI)

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -1,7 +1,19 @@
 // Aula0810.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+
+/*
+Comentario longo, bloco
+(mais de uma linha)
+*/
+
+
+#include "stdafx.h" // inclusao de biblioteca
+// C ANSI
+#include <stdio.h>
+#include <stdlib.h>
+
+
 
 
 int main()
