f13c89a27d324c5066728042c1f5626df29c15a8
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-08 09:34:36 -0300

    Variáveis (globais)

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -13,7 +13,11 @@ Comentario longo, bloco
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Variaveis (Globais, visiveis em todo o codigo)
+int a; // inteiro
+float b; // real (ponto flutuante)
+double c; // real (maior precisao)
+bool d; // booleano (verdadeiro/falso) (true/false)
 
 
 int main()
