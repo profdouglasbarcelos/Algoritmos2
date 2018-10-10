697f9e56169ed91daa6f32ae2cbfba67db69a746
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-10 11:32:32 -0300

    Adicionando o projeto Aula1010_CalculadoraSimples

---
+++ Aulas/Aula1010_CalculadoraSimples/Aula1010_CalculadoraSimples.cpp
@@ -0,0 +1,18 @@
+// Aula1010_CalculadoraSimples.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+char caractere;
+
+int main()
+{
+	caractere = '+';
+
+	printf("Olha soh: %c\n", caractere);
+	system("pause");
+
+    return 0;
+}
+
