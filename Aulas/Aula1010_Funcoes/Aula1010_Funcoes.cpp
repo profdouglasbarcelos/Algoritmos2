46a38b62075eb15a5c1ccbaec6bbc3607c15cd58
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-10 10:09:27 -0300

    Funcao com retorno int (Soma2Valores(int,int))

--- Aulas/Aula1010_Funcoes/Aula1010_Funcoes.cpp
+++ Aulas/Aula1010_Funcoes/Aula1010_Funcoes.cpp
@@ -9,9 +9,21 @@ void HelloWorld()
 	printf("Hello world!\n");
 }
 
+int Soma2Valores(int a, int b)
+{
+	/*int c;
+	c = a + b;
+	return c;*/
+	
+	return a + b;
+}
+
 int main()
 {
 	HelloWorld();
+	int d = Soma2Valores(99, 1);
+	printf("\tResultado: %i\n", d);
+
 	system("pause");
     return 0;
 }
