72948235c173fb68e90828d9e927d48e0cc48173
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-17 09:30:52 -0300

    Isolando a leitura de teclado em uma funcao

--- Aulas/Aula1010_CalculadoraSimples/Aula1010_CalculadoraSimples.cpp
+++ Aulas/Aula1010_CalculadoraSimples/Aula1010_CalculadoraSimples.cpp
@@ -25,25 +25,30 @@ float sub(float a, float b) {
 	return resultsub;
 }
 
+float LerNumeroTeclado()
+{
+	float valor;
+	printf_s("Digite um valor real:");
+	scanf_s("%f", &valor);
+	return valor;
+}
+
 int main(){
 	
-	setlocale(LC_ALL,"portuguese");
+	//setlocale(LC_ALL,"portuguese");
 
 	float a, b;
-
-	printf_s("digite o primeiro valor:\t");
-	scanf_s("%f", &a);
-	printf_s("digite o segundo valor:\t");
-	scanf_s("%f",&b);
+	a = LerNumeroTeclado();
+	b = LerNumeroTeclado();
 	
 	float resultadoSoma = soma(a, b);
-	printf_s("O valor da soma é: %f\n", resultadoSoma);
+	printf_s("O valor da soma eh: %f\n", resultadoSoma);
 	printf_s("%f foi o resultado da soma.\n", resultadoSoma);
 	printf_s("A soma de %f e %f eh: %f\n", a, b, resultadoSoma);
 
-	printf_s("O valor da subtração é: %f\n", sub(a, b));
-	printf_s("O valor a divisão é: %f\n", div(a, b));
-	printf_s("O valor da multiplicação é: %f\n", multi(a, b));
+	printf_s("O valor da subtracao eh: %f\n", sub(a, b));
+	printf_s("O valor a divisao eh: %f\n", div(a, b));
+	printf_s("O valor da multiplicacao eh: %f\n", multi(a, b));
 	
 	system("pause");
 }
