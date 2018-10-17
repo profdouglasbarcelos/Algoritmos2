963849cce71249d128675bf527ce3acce17f5673
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-17 09:17:55 -0300

    Codigo calculadora feita pelos alunos (dojo)

--- Aulas/Aula1010_CalculadoraSimples/Aula1010_CalculadoraSimples.cpp
+++ Aulas/Aula1010_CalculadoraSimples/Aula1010_CalculadoraSimples.cpp
@@ -4,15 +4,46 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
-char caractere;
 
-int main()
-{
-	caractere = '+';
-
-	printf("Olha soh: %c\n", caractere);
-	system("pause");
-
-    return 0;
+float soma(float a, float b) {
+	float resultsoma = a + b;
+	return resultsoma;
 }
+
+float multi(float a, float b) {
+	float resultmulti = a * b;
+	return resultmulti;
+}
+
+float div(float a, float b) {
+	float resultdiv = a / b;
+	return resultdiv;
+}
+
+float sub(float a, float b) {
+	float resultsub = a - b;
+	return resultsub;
+}
+
+int main(){
+
+	float a, b;
+
+	printf_s("digite o primeiro valor:\t");
+	scanf_s("%f", &a);
+	printf_s("digite o segundo valor:\t");
+	scanf_s("%f",&b);
+	
+	float resultadoSoma = soma(a, b);
+	printf_s("O valor da soma é: %f\n", resultadoSoma);
+	printf_s("%f foi o resultado da soma.\n", resultadoSoma);
+	printf_s("A soma de %f e %f eh: %f\n", a, b, resultadoSoma);
+
+	printf_s("O valor da subtração é: %f\n", sub(a, b));
+	printf_s("O valor a divisão é: %f\n", div(a, b));
+	printf_s("O valor da multiplicação é: %f\n", multi(a, b));
+	
+	system("pause");
+}
+
 
