106a46d8c498838b171f7899b5e91b5664da3189
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-22 10:26:46 -0300

    Funcao que encontra maior valor

--- Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
+++ Aulas/Aula2210_Condicionais/Aula2210_Condicionais.cpp
@@ -2,10 +2,52 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+
+int MaiorValorInteiro(int a, int b, int c, int d)
+{
+	int maior = a;
+	
+	if (maior < b)
+	{
+		maior = b;
+	}
+
+	if (maior < c)
+	{
+		maior = c;
+	}
+
+	if (maior < d)
+	{
+		maior = d;
+	}
+
+	return maior;
+}
+
+int LerValorInteiro()
+{
+	int valor = 0;
+	printf("Digite um numero inteiro: ");
+	scanf_s("%i", &valor);
+	return valor;
+}
 
 
 int main()
 {
+	int primeiro = LerValorInteiro();
+	int segundo = LerValorInteiro();
+	int terceiro = LerValorInteiro();
+	int quarto = LerValorInteiro();
+
+	int m = MaiorValorInteiro(primeiro, segundo, terceiro, quarto);
+
+	printf("O maior valor encontrado foi: %i\n\n", m);
+
+	system("pause");
+
     return 0;
 }
 
