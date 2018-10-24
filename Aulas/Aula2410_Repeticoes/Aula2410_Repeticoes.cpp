42f8f4ecc2ea4218fbcbaefab26f688141395789
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-24 11:39:03 -0300

    while, do while (exemplos)

--- Aulas/Aula2410_Repeticoes/Aula2410_Repeticoes.cpp
+++ Aulas/Aula2410_Repeticoes/Aula2410_Repeticoes.cpp
@@ -2,10 +2,52 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+
+
+
+
 
 
 int main()
 {
+	/* Loop infinito
+	while (true)
+	{
+		printf("aaa\n");
+	}*/
+
+	int a = 0;
+	while (a < 5)
+	{ // inicio escopo while
+		printf("%i\t", a);
+		
+		a++; // a = a + 1;
+
+	} // fim escopo while
+	
+	printf("\n ========================== \n");
+
+	a = 0;
+	while (a <= 5)
+	{ // inicio escopo while
+		printf("%i\t", a);
+
+		a++; // a = a + 1;
+
+		//a += 2; // a = a + 2;
+
+	} // fim escopo while
+
+	printf("\n ===== Do While =================== \n");
+
+	do
+	{
+		printf("%i\t", a);
+	} while (a <= 5);
+
+	printf("\n\n");
+	system("pause");
     return 0;
 }
 
