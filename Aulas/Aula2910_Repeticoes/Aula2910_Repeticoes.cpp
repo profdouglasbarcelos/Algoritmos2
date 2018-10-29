f7bfc819d1115d0a0a0e5e6cafe016a9e99a1a67
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2018-10-29 10:26:51 -0300

    InformarSeValoresEstaoEmOrdemCrescente (versao 7 variaveis)

--- Aulas/Aula2910_Repeticoes/Aula2910_Repeticoes.cpp
+++ Aulas/Aula2910_Repeticoes/Aula2910_Repeticoes.cpp
@@ -2,10 +2,99 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+
+int LerValorInteiro()
+{
+	int valor = 0;
+	printf("Digite um numero inteiro: ");
+	scanf_s("%i", &valor);
+	return valor;
+}
+
+void ImprimirSequenciaEntreInteiros()
+{
+	int n1 = LerValorInteiro();
+	int n2 = LerValorInteiro();
+
+	if (n1 <= n2)
+	{
+		int valorFinal = n2 - 1;
+		while (n1 < valorFinal)
+		{
+			n1++;
+			printf("%i\t", n1);
+		}
+	}
+	else
+	{
+		int valorFinal = n2 + 1;
+		while (n1 > valorFinal)
+		{
+			n1--;
+			printf("%i\t", n1);
+		}
+	}
+}
+
+void InformarSeValoresEstaoEmOrdemCrescente()
+{
+	int n1, n2, n3, n4, n5, numero;
+	int i = 0; // iterator, index (vetores)
+
+	while (i < 5)
+	{
+		numero = LerValorInteiro();
+
+		if (i == 0)
+		{
+			n1 = numero;
+		}
+		else if (i == 1)
+		{
+			n2 = numero;
+		}
+		else if (i == 2)
+		{
+			n3 = numero;
+		}
+		else if (i == 3)
+		{
+			n4 = numero;
+		}
+		else if (i == 4)
+		{
+			n5 = numero;
+		}
+
+		i++;
+	}
+
+	if ((n1 < n2) && (n2 < n3) && (n3 < n4) && (n4 < n5))
+	{
+		printf("Os valores estavam em ordem crescente");
+	}
+	else
+	{
+		printf("Os valores nao estavam em ordem crescente");
+	}
+}
+
 
 
 int main()
 {
-    return 0;
-}
+	//int a, b;
+	//scanf_s("%i %i", &a, &b);
+	//printf("A: %i, B: %i\n\n", a, b);
 
+	//ImprimirSequenciaEntreInteiros();
+	InformarSeValoresEstaoEmOrdemCrescente();
+
+
+
+
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
